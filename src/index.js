89e78c5f1bc8725f51fb2b74dd49c07a8083befd
ijosue16 <iradukundajosue16@gmail.com>
2023-01-25 08:13:19 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoContainer from './components/TodoContainer';
+import './App.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root'),
+);
+root.render(
+  <TodoContainer />,
+);
+// ReactDOM.render(<TodoContainer />, document.getElementById("root"))
